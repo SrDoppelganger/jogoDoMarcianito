a8af6090b71c0e4a1b064d757187e1bccf2d0a87
Arthur Albuquerque <arthur.albuquerque2004@gmail.com>
2025-03-12 10:40:52 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main
 		Random rand = new Random();
         Scanner scan = new Scanner(System.in);
         int tentativas = 0;
+        int limite = 7;
         
         //randomiza arvore entre 0 e 100
         int arvore = rand.nextInt(100) + 1;
@@ -20,32 +21,36 @@ public class Main
         //guarda o chute do jogador
         int guess = scan.nextInt();
         
-         gameLoop(arvore, guess, tentativas);
-         
-         gameEnding(arvore, guess, tentativas);
+         gameLoop(arvore, guess, tentativas, limite);
          
 	}
 	
-	public static void gameLoop(int arvore, int guess,int tentativas){
+	public static void gameLoop(int arvore, int guess,int tentativas,int limite){
 	   
 	   Scanner scan = new Scanner(System.in);
 	   
 	     while(guess != arvore){
                if(guess < arvore){
-                System.out.println("E Maior");
                 tentativas++;
+                System.out.println(tentativas +"/"+ limite);
+                System.out.println("E Maior");
             }
                else if(guess > arvore){
+                tentativas++;
+                System.out.println(tentativas +"/"+ limite);
                 System.out.println("E Menor");
-                tentativas++;  
             }
             
             System.out.println("Digite outro número entre 0 e 100");
             guess = scan.nextInt();
+            
+            if(guess == arvore){
+                gameEnding(arvore, guess, tentativas, limite);
+            }
 	     }
 	}
 	
-	public static void gameEnding(int arvore, int guess,int tentativas){
+	public static void gameEnding(int arvore, int guess,int tentativas, int limite){
 	    
 	     Scanner scan = new Scanner(System.in);
 	     Random rand = new Random();
@@ -59,7 +64,7 @@ public class Main
         if (escolha == 1){
             tentativas = 0;
             arvore = rand.nextInt(100) + 1;
-            gameLoop(arvore,guess,tentativas);
+            gameLoop(arvore,guess,tentativas,limite);
         }
         else{
          System.out.println("bye bye ^^");
