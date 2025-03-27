bf2d5a8b17248e4f2359ff9438a8ab659fd4000d
Arthur Albuquerque <arthur.albuquerque2004@gmail.com>
2025-03-27 17:17:15 -0300

    Ultima update do Main.java
    *aprimoramento do contator de tentativas
    *concerto de bugs em geral
    *add contador de menor numero de tentativas naquela sessão

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
+		int highscore = 10;
+		//começa o jogo
+        gameStart(highscore);
+	}
+	
+	public static void gameStart(int highscore){
 		Random rand = new Random();
         Scanner scan = new Scanner(System.in);
         int tentativas = 0;
@@ -12,62 +18,86 @@ public class Main
         //randomiza arvore entre 0 e 100
         int arvore = rand.nextInt(100) + 1;
         
-       
-        
-        //começa jogo
+		//começa jogo
         System.out.println("=== JOGO DO MARCIANO ===");
+        System.out.println("Um marciano caiu no planeta Terra e agora cabe a você a acha-lo\npara ajudar ele a voltar para sua terra natal");
         
         System.out.println("Digite um numero entre 0 e 100");
         //guarda o chute do jogador
         int guess = scan.nextInt();
         
-         gameLoop(arvore, guess, tentativas, limite);
-         
+        gameLoop(arvore, guess, tentativas, limite, highscore);
+        scan.close();
 	}
 	
-	public static void gameLoop(int arvore, int guess,int tentativas,int limite){
+	public static void gameLoop(int arvore, int guess,int tentativas,int limite, int highscore){
 	   
 	   Scanner scan = new Scanner(System.in);
 	   
 	     while(guess != arvore){
                if(guess < arvore){
                 tentativas++;
-                System.out.println(tentativas +"/"+ limite);
+                System.out.println("tentativas: " + tentativas +"/"+ limite);
                 System.out.println("E Maior");
             }
                else if(guess > arvore){
                 tentativas++;
-                System.out.println(tentativas +"/"+ limite);
+                System.out.println("tentativas: " + tentativas +"/"+ limite);
                 System.out.println("E Menor");
             }
+               if(tentativas > limite) {
+            	   gameEnding(arvore, guess, tentativas, limite, highscore);
+               }
             
             System.out.println("Digite outro número entre 0 e 100");
             guess = scan.nextInt();
             
             if(guess == arvore){
-                gameEnding(arvore, guess, tentativas, limite);
+                gameEnding(arvore, guess, tentativas, limite, highscore);
+                scan.close();
             }
 	     }
 	}
 	
-	public static void gameEnding(int arvore, int guess,int tentativas, int limite){
+	public static void gameEnding(int arvore, int guess,int tentativas, int limite, int highscore){
 	    
 	     Scanner scan = new Scanner(System.in);
 	     Random rand = new Random();
+	     highscore = checkScore(highscore, tentativas);
+	     
+	     if(tentativas<=limite) {
+	    	 System.out.println("Você acertou!!!");
+	         System.out.println("Tentativas: " + tentativas);
+	         System.out.println("Menor número de tentativas: " + highscore);
+	     }
+	     else {
+	    	 System.out.println("Você perdeu!!!");
+	         System.out.println("Tentativas: " + tentativas);
+	         System.out.println("Menor número de tentativas: " + highscore);
+	     }
 	    
-	     System.out.println("Você acertou!!!");
-         System.out.println("Tentativas: " + tentativas);
+	     
+	     //opção de jogar novamente
          System.out.println("Gostaria de jogar novamente?(1-sim/0-não)");
          int escolha = scan.nextInt();
-         
          
         if (escolha == 1){
             tentativas = 0;
             arvore = rand.nextInt(100) + 1;
-            gameLoop(arvore,guess,tentativas,limite);
+            gameStart(highscore);
         }
         else{
-         System.out.println("bye bye ^^");
+         System.out.println("até a próxima ^^");
         }
+	}
+	
+	public static int checkScore(int highscore, int tentativas){
+		if(highscore > tentativas) {
+			highscore = tentativas;
+			return tentativas;
+		}
+		else {
+			return highscore;
+		}	
 	}
 }
